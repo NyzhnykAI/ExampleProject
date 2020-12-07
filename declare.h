de3de954731ffd057cd011a5610c89abddec3274
NyzhnykAI <alex.nyzhnyk@gmail.com>
2020-12-07 01:11:48 -0800

    Second commit
    Add some code.

--- declare.h
+++ declare.h
@@ -1,6 +1,11 @@
 #ifndef __DECLARES__
 #define __DECLARES__
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBER 200
+
 // Function declaration
 void printToConsole();
 
--- main.c
+++ main.c
@@ -2,5 +2,7 @@
 
 int main() {
 	printf("Hello world");
+	int data = 100;
+	printf("\n%d\n", data);
 	return 0;
 }
